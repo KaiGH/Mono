d5a8fa809c7013b054f77bda2204ba9a0b4b4589
Kai Taylor <KaiGH@users.noreply.github.com>
2018-12-25 13:26:02 +0000

    Comments

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,10 +1,14 @@
 function navHamburger() 
 {
+    // Select the element
     var x = document.getElementById("topnav");
+
+    // Add the responsive class when user clicks menu button if its closed
     if (x.className === "topnav")
     {
         x.className += " responsive";
     }
+    // Remove the responsive class when user clicks menu button if its open
     else 
     {
         x.className = "topnav";
