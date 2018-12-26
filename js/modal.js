6724ccb75a2f7e9bb01357fd43b01b36f2dd8ba5
Kai Taylor <KaiGH@users.noreply.github.com>
2018-12-26 01:04:27 +0000

    Comments

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,15 @@
 function Show(idname)
 {
+    // Get DIV container by ID from HTML DOM
     var item = document.getElementById(idname)
 
+    // If the div has the hidden attribute then remove it
     if (item.hasAttributes('hidden'))
     {
        item.removeAttribute('hidden');
     }
 
+    // Scroll to the top with an animation to ensure user sees the modal
     $('html, body').animate(
     {
         scrollTop: 0
@@ -17,5 +20,6 @@ function Hide(idname)
 {
     var item = document.getElementById(idname)
 
+    // Create hidden attribute and set it to true to re-hide the modal
     item.setAttribute('hidden', true);
 }
